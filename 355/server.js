e242091b8bc2b3ed7223271b4a47851199ed48b6
Asadbek Karimov <asadbekasadbek@gmail.com>
2023-05-06 18:07:48 -0400

    bk
    upd

--- 355/server.js
+++ 355/server.js
@@ -3,6 +3,8 @@ var url = require('url');
 var fs = require('fs');
 var path = require('path');
 var crypto = require('crypto');
+const zlib = require('zlib');
+const querystring = require('querystring');
 
 var jobs = [];
 
@@ -25,32 +27,48 @@ http.createServer(function (req, res) {
         const fileName = req.headers['x-filename'] || `untitled-${hash}`; // get file name if not provided use the hash as file name
         const filePath = './tmp-uploads/' + fileName;
         const fileStream = fs.createWriteStream(filePath);
+        const chunks = [];
 
-        // save the data to file system
+        // save the data to file system && parse it for file info
         req.on('data', (chunk) => {
             fileStream.write(chunk);
+            chunks.push(chunk);
         });
 
         req.on('end', () => {
             // create a unique key value pair format { hash: filename}  and append to jobs
             jobs.push({task:`${hash}`, state: filePath});
             
+            // stiching all chunks
+            let formData = Buffer.concat(chunks);
+            
+            // decompressing formData
+            if (req.headers['content-encoding'] === 'gzip') {
+                zlib.gunzip(formData, (err, decoded) => {
+                    if (err) {
+                        res.statusCode(500);
+                        res.statusMessage('Failed at decompressing data')
+                    }
+                })
+            }
+            const parsedFormData = querystring.parse(formData.toString());
+            
+            console.log(parsedFormData);
             // send ok respond
             fileStream.end();
             
             // redirect user to google sigin to a
             const options = {
                 'method': 'POST',
-                'hostname': 'accounts.google.com',
-                'path': '/o/oauth2/auth',
+                'hostname': 'oauth2.googleapis.com',
+                'path': '/token',
                 'headers': {
                   'scope': 'https://www.googleapis.com/auth/drive',
                   'client_id': '669553241597-sjgkef797rcnuks50c3fo37eojrunsgq.apps.googleusercontent.com',
-                  'redirect_uri': 'https://oauth.pstmn.io/v1/callback',
+                  'redirect_uri': `http://${process.env.DOMAIN}/callback`,
                   'response_type': 'code',
                   'status': `${hash}`
-                },
-                'maxRedirects': 20
+                }
               };
 
               res.writeHead(302, { 'Location': options.headers.redirect_uri });
@@ -63,7 +81,7 @@ http.createServer(function (req, res) {
         return;
     }
 
-}).listen(8081);
+}).listen(3000);
 
 function generateShortHash() {
     const hash = crypto.randomBytes(6).toString('hex');
